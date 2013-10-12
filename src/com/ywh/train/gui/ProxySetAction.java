ad345b00ab0cc6f8c10cd982181bba160133070f
tjx222 <tjx1222@163.com>
2013-10-12 15:43:15 +0800

    complete proxy set
    完成代理属性设置

--- src/com/ywh/train/gui/ProxySetAction.java
+++ src/com/ywh/train/gui/ProxySetAction.java
@@ -4,19 +4,24 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
 
 import javax.swing.AbstractAction;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.text.MaskFormatter;
 
 import com.ywh.train.Config;
 import com.ywh.train.ResManager;
@@ -45,7 +50,7 @@ import com.ywh.train.ResManager;
 		Box vBox = Box.createVerticalBox();
 		vBox.setBorder(new TitledBorder(ResManager
 				.getString("RobTicket.panelProxyName"))); 
-		ButtonGroup group = new ButtonGroup();
+		final ButtonGroup group = new ButtonGroup();
 		
 		rbEnableProxy = new JRadioButton(ResManager
 				.getString("RobTicket.rbEnableProxy"),Config.isUseProxy());
@@ -63,14 +68,24 @@ import com.ywh.train.ResManager;
 		
 		Box hBox = Box.createHorizontalBox();
 		JLabel lbProxyIp = new JLabel(ResManager.getString("RobTicket.lbProxyIp")+":");
-		JTextField txtProxyIp = new JTextField(Config.getProxyIp(),16);
+		final JTextField txtProxyIp = new JTextField(Config.getProxyIp(),16);
 		hBox.add(lbProxyIp);
 		hBox.add(Box.createHorizontalStrut(32));
 		hBox.add(txtProxyIp);
 		
 		Box hBoxport = Box.createHorizontalBox();
 		JLabel lbProxyPort = new JLabel(ResManager.getString("RobTicket.lbProxyPort")+":");
-		JTextField txtProxyPort = new JTextField(Config.getProxyPort()+"",16);
+		MaskFormatter mf = null;
+		try {
+			mf = new MaskFormatter("*****");
+			mf.setValidCharacters(" 0123456789");
+			mf.setPlaceholder(" ");
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		
+		final JTextField txtProxyPort = new JFormattedTextField(mf);
+		txtProxyPort.setText(Config.getProxyPort()+"");
 		hBoxport.add(lbProxyPort);
 		hBoxport.add(Box.createHorizontalStrut(20));
 		hBoxport.add(txtProxyPort);
@@ -82,6 +97,35 @@ import com.ywh.train.ResManager;
 		JPanel plApply = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		btnApply = new JButton(ResManager.getString("RobTicket.btnApply"));
 		plApply.add(btnApply);
+		btnApply.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(!checkFormat(txtProxyIp.getText())){
+					JOptionPane.showConfirmDialog(dialog, ResManager.getString("RobTicket.msg.Ipfailture"),
+							ResManager.getString("RobTicket.msg.tip"),JOptionPane.OK_OPTION);
+					return;
+				}
+				Config.setUseProxy(group.isSelected(rbEnableProxy.getModel()));
+				Config.setProxyIp(txtProxyIp.getText().trim());
+				Config.setProxyPort(txtProxyPort.getText().trim());
+				if(Config.saveConfig()){
+					dialog.setVisible(false);
+				}else{
+					JOptionPane.showConfirmDialog(dialog, ResManager.getString("RobTicket.msg.failture"),
+							ResManager.getString("RobTicket.msg.tip"),JOptionPane.OK_OPTION);
+				}
+			}
+		
+		boolean checkFormat(String value){
+			String v = value.trim();
+			if(value.trim().length() > 0 &&
+				v.matches("((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)")){
+				return true;
+			}
+			return false;
+		}
+		}
+		);
 		
 		panelProxy.setBorder(new EmptyBorder(10, 10, 10, 10));
 		panelProxy.add(vBox,BorderLayout.NORTH);
